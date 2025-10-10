8c1262dd3a99d256b4eda149696a1100eb4a29a3
Jacob Pack <jacobpack@navapbc.com>
2025-10-10 13:59:24 -0400

    added container registry name limit as reference was failing due to character count exceed 50

--- infra/modules/azure/container-registry/data/main.tf
+++ infra/modules/azure/container-registry/data/main.tf
@@ -6,7 +6,8 @@ terraform {
   }
 }
 
+//Azure Container Registry names can't exceed 50 characters
 data "azurerm_container_registry" "registry" {
-  name                = var.name
+  name                = substr(var.name, 0, 49)
   resource_group_name = var.resource_group_name
 }
