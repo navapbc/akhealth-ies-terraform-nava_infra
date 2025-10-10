91993e18aada2ccf7519104630d55d97587e9287
Jacob Pack <jacobpack@navapbc.com>
2025-10-10 16:58:28 -0400

    fixed container registry name substring reference and removed old fix

--- infra/modules/azure/container-registry/data/main.tf
+++ infra/modules/azure/container-registry/data/main.tf
@@ -6,8 +6,7 @@ terraform {
   }
 }
 
-//Azure Container Registry names can't exceed 50 characters
 data "azurerm_container_registry" "registry" {
-  name                = substr(var.name, 0, 49)
+  name                = var.name
   resource_group_name = var.resource_group_name
 }
