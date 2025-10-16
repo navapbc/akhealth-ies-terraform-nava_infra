56794a4c59c17ee77708b68a9d3b7f41cc1620aa
Jacob Pack <jacobpack@navapbc.com>
2025-10-16 12:17:59 -0400

    added GH action info

--- infra/project-config/main.tf
+++ infra/project-config/main.tf
@@ -13,6 +13,10 @@ locals {
   tenant_id = "ba06645f-e0cc-44b5-897f-34eb6aa59588"
 
   github_actions_azure_config = {
+    "Alaska IES" : {
+      "client_id" = "270618bf-24fb-414e-85fb-d76d4b48956f",
+      "object_id" = "3ce677e5-9014-4638-b9c4-36d451d63111"
+    }
     # lowers : {
     #   client_id : "<client_id>",
     #   object_id : "<object_id>",
@@ -25,6 +29,21 @@ locals {
   }
 
   infra_admins = {
+
+     "Alaska IES" : {
+       object_ids : [
+         "2f297620-817c-4712-8dc3-413adc5b90c6", //infrastructure admins
+         local.github_actions_azure_config["Alaska IES"].object_id,
+       ]
+     },
+       "Alaska IES" : {
+       object_ids : [
+         "ae9e09d1-76a2-4275-a36c-e92a28256985", //app registration
+         local.github_actions_azure_config["Alaska IES"].object_id,
+       ]
+     }
+
+
     # lowers : {
     #   object_ids : [
     #     "<user_object_id>",
@@ -42,5 +61,5 @@ locals {
 
   default_certificate_contact_email = "jacobpack@navapbc.com"
 
-  
+
 }
